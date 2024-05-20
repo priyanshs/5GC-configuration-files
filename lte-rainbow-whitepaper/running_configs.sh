765e326c6766328e855f998640565e3e0011827a
priyanshs <priyanshsingh@outlook.com>
2024-05-20 13:11:58 +0530

    session allocation errors

---
+++ lte-rainbow-whitepaper/running_configs.sh
@@ -0,0 +1,7 @@
+open5gs-mmed -c mme.yaml &
+open5gs-sgwcd -c sgwc.yaml &
+open5gs-smfd -c smf.yaml &
+open5gs-hssd -c hss.yaml &
+open5gs-pcrfd -c pcrf.yaml &
+open5gs-sgwud -c sgwu.yaml &
+open5gs-upfd -c upf.yaml &
